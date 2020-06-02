11249a7dd77dad8386d34abd0911e136129773c7
Andrew <niuniup10@gmail.com>
2020-06-02 10:28:46 -0600

    implemented register function

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,11 +8,26 @@ exports.login = function() {
 
 exports.logout = function() {
 
-}  
+} 
 
-exports.register = function() {
+
+
+exports.register = function(req, res) {
     let user = new User(req.body)
-    user.register()
+    user.register().then(() => {
+        req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id}
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    }).catch((regErrors) => {
+        regErrors.forEach(function(error) {
+            req.flash('regErrors', error)
+        })
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    })
+   
 }  
 
 exports.home = function(req, res) {
--- models/User.js
+++ models/User.js
@@ -1,9 +1,91 @@
+const validator = require("validator")
+
 let User = function () {
     this.data = data
+    this.errors = []
 }
 
-User.prototype.register = function() {
+User.prototype.cleanUp = function() {
+    if (typeof(this.data.username) != "string") {
+        this.data.user = ""
+    }
+    if (typeof(this.data.email) != "string") {
+        this.data.email = ""
+    }
+    if (typeof(this.data.password) != "string") {
+        this.data.password = ""
+    }
+
+    // get rid of any bogus properties
+    this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        email: this.data.email.trim().toLowerCase(),
+        password: this.data.password
+    }
     
+}
+
+User.prototype.validate = function() {
+    return new Promise(async (resolve, reject) => {
+        if (this.data.username == "") {
+            this.errors.push("You must provide a username")
+        }
+        if (this.data.user != "" && !validator.isAlphanumeric(this.data.username)) {
+            this.errors.push("User name can only contain letters and numbers")
+        }
+        if (!validator.isEmail(this.data.email)) {
+            this.errors.push("You must provide a valid email")
+        }
+        if (this.data.password == "") {
+            this.errors.push("You must provide a valid password")
+        }
+        if (this.data.password.length > 0 && this.data.password.length < 12) {
+            this.errors.push("Password must be at least 12 chars")
+        }
+        if (this.data.password.length > 50) {
+            this.errors.push("Password too long 50")
+        }
+        if (this.data.username.length > 0 && this.data.password.length < 3) {
+            this.errors.push("Username must be at least 3 chars")
+        }
+        if (this.data.username.length > 30) {
+            this.errors.push("Username too long 30")
+        }
+    
+        // valid username, check if it is taken
+        if (this.data.username > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
+            let usernameExists = await usersCollection.findOne({username: this.data.username})
+            if (usernameExists) {this.errors.push("Username already taken")}
+        }
+    
+        if (validator.isEmail(this.data.email)) {
+            let emailExists = await usersCollection.findOne({email: this.data.email})
+            if (emailExists) {this.errors.push("Email already taken")}
+        }
+
+        resolve()
+    })
+}
+
+
+User.prototype.register = function() {
+    return new Promise(async (resolve, reject) => {
+        // validate data
+        this.cleanUp()
+        await this.validate()
+        //no error, then save to database
+    
+        if (!this.errors.length) {
+            // hash userpassword
+            let salt = bcrypt.genSaltSync(10)
+            this.data.password = bcrypt.hashSync(this.data.password, salt)
+            await usersCollection.insertOne(this.data)
+            this.getAvatar()
+            resolve()
+        } else {
+            reject(this.errors)
+        }
+    })
 }
 
 module.exports = User
